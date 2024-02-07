bfb0760710b4bfb6574792ab85f7a880b034fabb
Clearwave <fedoraerb@gmail.com>
2024-02-07 17:48:06 +0800

    Add files via upload

--- Server/Data/Data_Process.py
+++ Server/Data/Data_Process.py
@@ -15,10 +15,13 @@ for inFile in os.listdir():
         
         data[str(cnt)] = {'in':inData,'out':outData,'score':100//data_cnt}
 
-print(json.dumps({
-    'pid': pid,
-    'time_limit': timeLimit,
-    'mem_limit': memLimit,
-    'data_cnt': data_cnt,
-    'data': data
-}))
+with open(f"{pid}.json",'w') as f:
+    f.write(json.dumps({
+        'pid': pid,
+        'time_limit': timeLimit,
+       'mem_limit': memLimit,
+        'data_cnt': data_cnt,
+        'data': data
+    }))
+
+print ('Done.')
